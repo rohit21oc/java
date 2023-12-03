dcfde36973b107f0b7cba86caa59310a25686b37
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-04 00:16:27 +0530

    Add files via upload

---
+++ data_types.java
@@ -0,0 +1,18 @@
+public class data_types {
+    public static void main(String[] args) {
+        byte b=1;
+        System.out.println(b);
+        boolean var = false;
+        char ch = 'R';
+        System.out.println(ch);
+        System.out.println(var);
+        float price = 15;
+        System.out.println(price);
+        int num=7;
+        System.out.println(num);
+        short n =240;
+        System.out.println(n);
+        double big = 1256.12;
+        System.out.println(big);
+    }
+}
---
+++ first.java
@@ -0,0 +1,8 @@
+public class first {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("Hello World\n");
+        System.out.print("Hello World\n");
+    }
+}
