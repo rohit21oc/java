d19584601507c2d8bfdb589803df4cf9611e606e
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-06 23:51:05 +0530

    Add files via upload

---
+++ one_to_ten.java
@@ -0,0 +1,10 @@
+public class one_to_ten {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count <=10) {
+            System.out.println(count);
+            count++;
+            
+        }
+    }
+}
