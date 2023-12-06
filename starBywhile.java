27c02c2af34f255fc69eeef57e35ded82273a2ff
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-06 23:50:48 +0530

    Add files via upload

---
+++ starBywhile.java
@@ -0,0 +1,9 @@
+public class starBywhile {
+    public static void main(String[] args) {
+        int line = 1;
+        while (line<=4) {
+            System.out.println("****");
+            line++;
+        }
+    }
+}
