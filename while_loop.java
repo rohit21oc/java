29ad92d106b2db8e0a831cb71b57fbc9e9657a6a
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-06 23:50:35 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,11 @@
+public class while_loop {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter <= 100000) {
+System.out.println(counter+" Hello World");  
+counter++;          
+        }
+        System.out.println("It printed 100 time hello world");
+    }
+}
