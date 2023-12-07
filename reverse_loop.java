f199489f16f086a0466819c94267c8979763ff63
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-07 23:58:33 +0530

    Add files via upload

---
+++ reverse_loop.java
@@ -0,0 +1,12 @@
+public class reverse_loop {
+    public static void main(String[] args) {
+        int n = 10899;
+        while (n>0) {
+            int lastDigit = n % 10;
+            System.out.print(lastDigit);
+            n /=10;
+        }
+        System.out.println();
+    }
+    
+}
