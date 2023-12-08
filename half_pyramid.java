5b834b31c8f575b0aadede90f830cfb6ee6db4a0
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-08 23:40:16 +0530

    Add files via upload

---
+++ half_pyramid.java
@@ -0,0 +1,12 @@
+public class half_pyramid {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int line=1;line<=n;line++){
+            for(int number=1;number<=line;number++){
+                System.out.print(number);
+                
+            }
+            System.out.println();
+        }
+    }
+}
