4126a7b85a1d806c9518f585b174b47ad08654cd
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-08 23:37:45 +0530

    Add files via upload

---
+++ do_while_loop.java
@@ -0,0 +1,9 @@
+public class do_while_loop {
+    public static void main(String[] args) {
+        int count = 1;
+        do{
+            System.out.println("Hello World");
+            count++;
+        }while(count<=10);
+    }
+}
