17e7513d5ee4fbe78b5e8692ff973febf382c0e2
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-09 21:15:01 +0530

    Add files via upload

---
+++ fn_multiply.java
@@ -0,0 +1,14 @@
+public class fn_multiply {
+    public static int multiply(int a,int b){
+        int product=a*b;
+        return product;
+
+    }
+    public static void main(String[] args) {
+        // int a = 5;
+        // int b=10;
+        // int prod = multiply(a, b);
+        // System.out.println("your product is "+ prod);
+        System.out.println("Your product is "+multiply(10, 5));
+    }
+}
