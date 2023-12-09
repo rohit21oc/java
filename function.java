fab731b78a1234912c0734104b53c8ab053018c0
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-09 21:14:49 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,14 @@
+public class function{
+    public static void printHelloWorld(){
+        // System.out.println("Hello World");
+        // System.out.println("Hello World");
+        // System.out.println("Hello World");
+        for(int i=1;i<=10;i++){
+            System.out.println(i +" Hello World");
+        }
+        return;
+    }
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}
