435f8decf1a91bd45eb2d6fc9e775c160f0567e2
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-10 23:58:14 +0530

    Add files via upload

---
+++ operators/arithmetic_op.java
@@ -0,0 +1,11 @@
+public class arithmetic_op {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println("Addition:"+(A+B));
+        System.out.println("Substraction:"+(A-B));
+        System.out.println("Division:"+(A/B));
+        System.out.println("Multiplication:"+(A*B));
+        System.out.println("Remender:"+(A%B));
+    }
+}
---
+++ operators/logical.java
@@ -0,0 +1,7 @@
+public class logical {
+    public static void main(String[] args) {
+        System.out.println((10>4)&&(10<11));
+        System.out.println((10>4)||(10>12));
+        System.out.println(!((10>12)&&(10<11)));
+    }
+}
