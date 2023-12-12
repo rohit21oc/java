cfceb3540c146f15e9e4b6e3ef0eea03830eca34
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-12 23:54:24 +0530

    Add files via upload

---
+++ binToDec.java
@@ -0,0 +1,18 @@
+public class binToDec {
+    public static void biToDec(int binNum){
+        int myNum = binNum;
+        int pow=0;
+        int decNum=0;
+        while (binNum>0) {
+            int lastDigit = binNum%10;
+
+            decNum = decNum +(lastDigit *(int)Math.pow(2,pow));
+            pow++;
+            binNum = binNum/10;
+        }
+        System.out.println("Decimal of "+myNum+" = "+decNum);
+    }
+    public static void main(String[] args) {
+        biToDec(101);
+    }
+}
