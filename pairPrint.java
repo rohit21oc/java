76214f477f92986470e6786fa54e2f6f68686ffb
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2024-01-09 18:35:29 +0530

    Add files via upload

---
+++ pairPrint.java
@@ -0,0 +1,21 @@
+// package revision;
+
+public class pairPrint {
+    public static void printPrairs(int number[]){
+        int tp = 0;
+        for(int i=0;i<number.length;i++){
+            int curr = number[i];
+            for(int j=i+1;j<number.length;j++){
+                System.out.print( "(" + curr + ","+ number[j]+")");
+                tp++;
+            }
+            System.out.println();
+            System.out.println(tp);
+        }
+    }
+    public static void main(String[] args) {
+        int number[] = {2,4,6,8,10,12,14};
+        printPrairs(number);
+        
+    }
+}
